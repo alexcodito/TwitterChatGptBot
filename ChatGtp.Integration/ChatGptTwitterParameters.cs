686477c09c1002ea39518349273bb14a0f689f8f
alexcodito <alexcodito@users.noreply.github.com>
2023-02-04 15:07:40 +0000

    Generate tweet from topic

---
+++ ChatGtp.Integration/ChatGptTwitterParameters.cs
@@ -0,0 +1,30 @@
+﻿using OpenAI.Models;
+
+namespace TwitterChatGptBot.ChatGpt.Integration
+{
+    public sealed class ChatGptTwitterParameters
+    {
+        private Model _model = Model.Ada;
+        private double? _temperature = 0.1;
+
+        public ChatGptTwitterParameters(Model model, double temperature)
+        {
+            _model = model;
+            _temperature = temperature;
+        }
+
+        public void SetModel(Model model)
+        {
+            _model = model;
+        }
+
+        public void SetTemperature(double? temperature)
+        {
+            _temperature = temperature;
+        }
+
+        public Model Model => _model;
+
+        public double? Temperature => _temperature;
+    }
+}
